e62672549b9173888f21a6c92c020fb7a71d538d
Mukhammad339 <Mukhammad339@gmail.com>
2026-03-07 01:45:08 +0300

    Add API handler to fetch and return VPN keys

---
+++ api/sub.js
@@ -0,0 +1,36 @@
+export default async function handler(req, res) {
+
+const project = "vpn-sub-910e3";
+
+const url =
+`https://firestore.googleapis.com/v1/projects/${project}/databases/(default)/documents/vpn_keys`;
+
+const r = await fetch(url);
+const data = await r.json();
+
+let list = [];
+
+if (data.documents) {
+
+for (let doc of data.documents) {
+
+const fields = doc.fields;
+
+if (fields.key) {
+list.push(fields.key.stringValue);
+}
+
+if (fields.link) {
+list.push(fields.link.stringValue);
+}
+
+}
+
+}
+
+const text = list.join("\n");
+
+res.setHeader("content-type","text/plain");
+res.send(text);
+
+}
